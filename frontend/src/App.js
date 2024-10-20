fec29256e9385feb8588ab1391ed1cf536998ee9
rding12 <rachelding@microvu.com>
2024-10-19 20:26:08 -0700

    Implemented routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Router components
 import './App.css';
 import HomePage from './HomePage';
 import Teacher from './components/Teacher';
 
 function App() {
   return (
-   // <HomePage />
-   <Teacher/>
+    <Router>
+      <Routes>
+        {/* Define routes for different pages/components */}
+        <Route path="/" element={<HomePage />} /> {/* Home Page route */}
+        <Route path="/teacher" element={<Teacher />} /> {/* Teacher Page route */}
+      </Routes>
+    </Router>
+
   );
 }
 
