8ec20ba61e6d788b067931181394fb6623d41417
Purav Patel <purav001@gmail.com>
2024-10-20 05:24:20 -0700

    Implemented New Login Button

--- frontend/src/components/LoginButton.jsx
+++ frontend/src/components/LoginButton.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import EncryptButton from "./EncryptButton";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
   return (
-    <button
-      onClick={() =>
+    <EncryptButton
+      handleClick={() =>
         loginWithRedirect({
           redirectUri: `${window.location.origin}/teacher`
         })
       }
-    >
-      Log In
-    </button>
+    />
   );
 };
 
